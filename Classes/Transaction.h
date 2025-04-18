08342ec92abfe0bdbd70d9ca3f0cc638384b7658
ahmed ali mahdy <ahmedali.clash3@gmail.com>
2025-04-18 18:29:17 +0200

    Add transaction.h

--- Classes/Transaction.h
+++ Classes/Transaction.h
@@ -1,1 +1,52 @@
 #pragma once
+#ifndef Transaction_H
+#define TRansaction_H
+#include <string>
+#include <chrono>
+using namespace std;
+enum class TRANSACTION_TYPE {
+    SEND_MONEY,
+    REQUEST_MONEY,
+    DEPOSIT,
+    WITHDRAWAL,
+
+};
+
+enum class RequestStatus {
+    NONE,
+    PENDING,
+    ACCEPTED,
+    DECLINED,
+
+};
+
+class transaction {
+private:
+    string sender;
+    string recipient;
+    double amount;
+    time_t timestamp;
+    TRANSACTION_TYPE type;
+    RequestStatus status;
+
+public:
+
+    WalletEvent(const std::string& from, const std::string& to, double amount,
+        TRANSACTION_TYPE t, RequestStatus s = RequestStatus::NONE);
+
+    // Getters
+    string getSender() const;
+    string getRecipient() const;
+    double getAmount() const;
+    string getTimestampAsString() const;
+    TRANSACTION_TYPE getType() const;
+    RequestStatus getStatus() const;
+
+    // Setters
+    void setStatus(RequestStatus newStatus);
+
+    // Utility functions
+    string getTypeString() const;
+    string getStatusString() const;
+};
+#endif
