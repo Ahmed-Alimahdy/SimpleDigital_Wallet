8538760a2486b0a29ab757859ddbb22318e91915
ahmedebrahim12 <ahmedebrahim200515@gmail.com>
2025-04-19 12:36:02 +0200

    Add Admin.h code

---
+++ Classes/Admin.cpp
@@ -0,0 +1,6 @@
+#include "Admin.h"
+
+void Admin::viewAllUsers()
+{
+
+}
--- Classes/Admin.h
+++ Classes/Admin.h
@@ -1,1 +1,21 @@
 #pragma once
+#include <string>
+#include "User.h"
+class Admin {
+    string username, password;
+
+    //void adminDashboard();
+    void viewAllUsers();
+    user findUser(); //user class behaviour
+
+    void addUser();
+    void viewUserData();
+    void deleteUser(user);
+    void updateUserData(user);
+    void suspendUser(user);
+
+    void viewAllTransactions();
+
+    void logout();
+    void addAdmin();
+};
